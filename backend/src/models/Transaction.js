70f6828d30379cf5c3e0bf9af9b96ae8b48f7e51
Omkar Patil <ompatil5132@gmail.com>
2026-04-04 21:31:51 +0530

    transaction schema

---
+++ backend/src/models/Transaction.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+  amount: { 
+    type: Number,
+    required: true 
+},
+  type: { 
+    type: String, 
+    enum: ['income', 'expense'], 
+    required: true 
+},
+  category: { 
+    type: String, 
+    required: true, 
+    trim: true 
+  },
+  date: { 
+    type: Date, 
+    default: Date.now 
+  },
+  notes: { 
+    type: String, 
+    trim: true 
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Transaction', transactionSchema);
