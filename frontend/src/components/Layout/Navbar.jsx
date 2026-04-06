5259b0ecfa0d5963a2691e38bbc7e312d4d95bee
Omkar Patil <ompatil5132@gmail.com>
2026-04-06 13:09:05 +0530

    Navbar

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -1,9 +1,18 @@
 import { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import { LogOut } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
   const { user, logout } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+  const handleLogout = () => {
+    if (window.confirm('Are you sure you want to logout?')) {
+      logout();
+      navigate('/login');
+    }
+  };
 
   return (
     <nav className="bg-card border-b border-white/10 px-8 py-5 flex items-center justify-between">
@@ -18,10 +27,10 @@ const Navbar = () => {
           <span className="capitalize font-medium text-primary">{user?.role}</span>
         </div>
         <button
-          onClick={logout}
-          className="flex items-center gap-2 text-black-200 hover:text-white transition-colors"
+          onClick={handleLogout}
+          className="flex items-center gap-2 text-black-200 hover:text-red-500 transition-colors"
         >
-          <LogOut size={18} />
+          <LogOut size={20} />
           Logout
         </button>
       </div>
@@ -30,3 +39,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
