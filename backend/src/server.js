c036ed3660f196c9fe56451d4ab8079dd23935a8
Omkar Patil <ompatil5132@gmail.com>
2026-04-06 17:20:56 +0530

    cors setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://finance-dashboard-dusky-rho-23.vercel.app','https://finance-dashboard-dusky-rho-23.vercel.app/login'],
+  origin: ['http://localhost:5173', 'https://finance-dashboard-dusky-rho-23.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
